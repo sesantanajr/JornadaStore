b93ef7b3402d08600b168448159d2834eb0bd757
Sérgio Sant'Ana Júnior <sesantanajr@gmail.com>
2024-09-20 13:00:03 -0300

    BlockStore.ps1

--- BlockStore.ps1
+++ BlockStore.ps1
@@ -14,24 +14,6 @@
 # ===============================================
 
 try {
-    # Verifica se o script está sendo executado com permissões de administrador
-    if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-        throw "O script deve ser executado com permissões de administrador."
-    }
-
-    # Caminho da GPO para bloquear a Microsoft Store (via registro)
-    $gpoPath = "HKLM:\Software\Policies\Microsoft\WindowsStore"
-    $gpoName = "RemoveWindowsStore"
-    $gpoValue = 1
-
-    # Cria o caminho da política se ele não existir
-    if (-not (Test-Path $gpoPath)) {
-        New-Item -Path $gpoPath -Force
-    }
-
-    # Bloqueia a Store via política de grupo
-    Set-ItemProperty -Path $gpoPath -Name $gpoName -Value $gpoValue
-    Write-Host "Política de bloqueio da Microsoft Store aplicada com sucesso."
 
     # Habilita atualizações automáticas dos aplicativos da Microsoft Store
     $autoUpdatePath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsStore\WindowsUpdate"
@@ -40,21 +22,26 @@ try {
     }
     Set-ItemProperty -Path $autoUpdatePath -Name "AutoDownload" -Value 4  # Habilita atualizações automáticas
     Write-Host "Atualizações automáticas dos aplicativos da Store habilitadas."
+    # Define the registry path
+    $registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\WindowsStore"
 
-    # Regras de Firewall para bloquear o tráfego da Microsoft Store
-    Write-Host "Aplicando regras de firewall para bloquear o tráfego da Microsoft Store..."
+    # Bloqueia a Store por chave de registro
+    $registryName = "RequirePrivateStoreOnly"
+    $registryValue = 1
 
-    # Cria uma regra para bloquear a Store
-    netsh advfirewall firewall add rule name="Block Microsoft Store" dir=out action=block remoteip=13.107.4.50,13.107.5.50 enable=yes
+    # Verifica a existência do registro e os cria se não existir
+    if (-not (Test-Path $registryPath)) {
+        New-Item -Path $registryPath -Force
+    }
 
-    Write-Host "Regras de firewall aplicadas com sucesso."
+    # Define o valor da chave de registro
+    Set-ItemProperty -Path $registryPath -Name $registryName -Value $registryValue -Type DWord
 
+    Write-Host "Chave de registro definida com sucesso."
 } catch {
-    Write-Host "Erro ao aplicar a política de bloqueio: $_"
-    $error | Out-File -FilePath "C:\Logs\MicrosoftStore_Block_Firewall_Error.log" -Append
-} finally {
-    Write-Host "Processo de bloqueio concluído."
+    Write-Host "Ocorreu um erro: $_"
 }
+
 
 # ===============================================
 # Fim do Script
